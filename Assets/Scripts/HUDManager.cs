c1e40987790aa0ebad9a5fe2de13ab996913685c
fjortegan <fjortegan@gmail.com>
2023-10-15 17:03:04 +0200

    gamemanager popup text UI

--- Assets/Scripts/HUDManager.cs
+++ Assets/Scripts/HUDManager.cs
@@ -12,7 +12,6 @@ public class HUDManager : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         _image = GetComponent<Image>();
-        _image.color = Color.green;
     }
 
     // Update is called once per frame
